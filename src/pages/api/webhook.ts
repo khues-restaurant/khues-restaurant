0a67001e235df1f28f25f24f0f78b2373cdf270e
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-03-10 14:28:18 -0500

    respects new prisma normalized schema + decent amount of validation

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -3,8 +3,11 @@ import { buffer } from "micro";
 import { env } from "~/env";
 import Stripe from "stripe";
 import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 
 import { socket } from "~/pages/_app";
+import { OrderDetails } from "~/stores/MainStore";
+import { orderDetailsSchema } from "~/server/api/routers/payment";
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
   apiVersion: "2023-10-16",
@@ -16,145 +19,229 @@ export const config = {
   },
 };
 
+const PaymentMetadataSchema = z.object({
+  userId: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string().email(), // This validates that the email field contains a valid email address
+  phoneNumber: z.string(), // Additional validation can be added if there's a specific format for phone numbers
+});
+
+interface PaymentMetadata {
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+}
+
+// TODO: clean up all of the early returns and try to maybe consolidate them a bit?
+
 const webhook = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("hit at all!");
+  console.log("webhook hit");
 
-  if (req.method === "POST") {
-    const buf = await buffer(req);
-    const sig = req.headers["stripe-signature"] as string;
-    let event;
-
-    try {
-      event = stripe.webhooks.constructEvent(
-        buf,
-        sig,
-        "whsec_6efd9dc6493e04a4078e8e004d174c4a15e6aa678ba464b60fb257eb710a0925", // TODO: replace with process.env.STRIPE_WEBHOOK_SECRET
-      );
-    } catch (err) {
-      let message = "Unknown Error";
-      if (err instanceof Error) message = err.message;
-      res.status(400).send(`Webhook Error: ${message}`);
-      return;
-    }
-    switch (event.type) {
-      case "checkout.session.completed":
-        const paymentIntentSucceeded = event.data.object;
-
-        console.dir(paymentIntentSucceeded);
-
-        if (
-          paymentIntentSucceeded.metadata === null ||
-          paymentIntentSucceeded.amount_total === null
-        ) {
-          console.log("returning early");
-          res.json({ received: true });
-          return;
-        }
-
-        const prisma = new PrismaClient();
-
-        // 1) get user object (if exists) from user table
-        const user = await prisma.user.findFirst({
-          where: {
-            id: paymentIntentSucceeded.metadata.userId,
-          },
-        });
-
-        // 2) get transient order details
-        const transientOrder = await prisma.transientOrder.findFirst({
-          where: {
-            userId: paymentIntentSucceeded.metadata.userId,
-          },
-        });
-
-        if (!transientOrder) {
-          console.log("No transient order found for user");
-          res.json({ received: true });
-          return;
-        }
-
-        const prevPoints = user?.rewardsPoints ?? 0;
-        const prevRank = user?.rewardsRank ?? 1;
-        let currPoints = 0;
-        let currRank = 1;
-
-        // calculate new rewards points/rank from order
-        const totalPrice = paymentIntentSucceeded.amount_total;
-        const pointsEarned = Math.floor(totalPrice / 10);
-
-        currPoints = prevPoints + pointsEarned;
-
-        if (user) {
-          // calculate new rank if applicable
-          if (currPoints > 1000 && currRank < 5) {
-            currRank++;
-            currPoints = currPoints - 1000;
-          }
-        }
-
-        // 3) create order row
-        const order = await prisma.order.create({
-          data: {
-            firstName: paymentIntentSucceeded.metadata.firstName!,
-            lastName: paymentIntentSucceeded.metadata.lastName!,
-            email: paymentIntentSucceeded.metadata.email!,
-            phoneNumber: paymentIntentSucceeded.metadata.phoneNumber!,
-            stripeSessionId: paymentIntentSucceeded.id, // idk is this the right field?
-
-            prevRewardsPoints: prevPoints,
-            rewardsPoints: currPoints,
-            prevRewardsRank: prevRank,
-            rewardsRank: currRank,
-
-            // @ts-expect-error fix typing later
-            details: transientOrder.details as unknown as Record<
-              string,
-              unknown
-            >,
-            userId: user ? user.userId : null,
-          },
-        });
-
-        // 4) update user rewards points/rank
-        if (user) {
-          await prisma.user.update({
-            where: {
-              id: paymentIntentSucceeded.metadata.userId,
-            },
-            data: {
-              rewardsPoints: currPoints,
-              rewardsRank: currRank,
-            },
-          });
-        }
-
-        // 5) send websocket emit to dashboard
-
-        // seems like we may have to do some extra work to get this to send to
-        // backend socket.ts
-        socket.emit("newOrderCreated");
-
-        // TODO
-
-        // 6) send post request to w/e pos system we are using
-
-        // TODO
-
-        await prisma.transientOrder.delete({
-          where: {
-            userId: paymentIntentSucceeded.metadata.userId,
-          },
-        });
-
-        break;
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
-    res.json({ received: true });
-  } else {
+  if (req.method !== "POST") {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method Not Allowed");
+    return;
   }
+
+  const buf = await buffer(req);
+  const sig = req.headers["stripe-signature"] as string;
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      buf,
+      sig,
+      "whsec_6efd9dc6493e04a4078e8e004d174c4a15e6aa678ba464b60fb257eb710a0925", // TODO: replace with process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (err) {
+    let message = "Unknown Error";
+    if (err instanceof Error) message = err.message;
+    res.status(400).send(`Webhook Error: ${message}`);
+    return;
+  }
+
+  switch (event.type) {
+    case "checkout.session.completed":
+      const payment = event.data.object;
+
+      console.dir(payment);
+
+      if (payment.metadata === null || payment.amount_total === null) {
+        console.log("returning early");
+        res.json({ received: true });
+        return;
+      }
+
+      let paymentMetadata: PaymentMetadata | undefined = undefined;
+
+      try {
+        // This will throw an error if the object does not match the schema
+        paymentMetadata = PaymentMetadataSchema.parse(payment.metadata);
+      } catch (error) {
+        console.error("Validation failed:", error);
+      }
+
+      if (paymentMetadata === undefined) {
+        console.log("returning early");
+        res.json({ received: true });
+        return;
+      }
+
+      const prisma = new PrismaClient();
+
+      // 1) get user object (if exists) from user table
+      const user = await prisma.user.findFirst({
+        where: {
+          id: payment.metadata.userId,
+        },
+      });
+
+      // 2) get transient order details
+      const transientOrder = await prisma.transientOrder.findFirst({
+        where: {
+          userId: payment.metadata.userId,
+        },
+      });
+
+      if (!transientOrder) {
+        console.log("No transient order found for user");
+        res.json({ received: true });
+        return;
+      }
+
+      let orderDetails: OrderDetails | undefined = undefined;
+
+      try {
+        // This will throw an error if the object does not match the schema
+        // @ts-expect-error fix "date as string" types soon
+        orderDetails = orderDetailsSchema.parse(transientOrder.details);
+      } catch (error) {
+        console.error("Validation failed:", error);
+      }
+
+      if (orderDetails === undefined) {
+        console.log("returning early");
+        res.json({ received: true });
+        return;
+      }
+
+      const prevPoints = user?.rewardsPoints ?? 0;
+      let currPoints = 0;
+
+      // calculate new rewards points from order
+      const totalPrice = payment.amount_total;
+      const pointsEarned = Math.floor(totalPrice / 10);
+      let showRewardsDiscountNotification = false;
+
+      currPoints = prevPoints + pointsEarned;
+
+      if (user) {
+        // create new reward discount if threshold is met
+        if (currPoints > 1500) {
+          const now = new Date();
+          const twoMonthsFromNow = new Date(
+            now.getFullYear(),
+            now.getMonth() + 2,
+            now.getDate(),
+          );
+
+          // needs to be awaited or no?
+          await prisma.discount.create({
+            data: {
+              name: "Points",
+              description: "1500 point free meal reward",
+              expirationDate: twoMonthsFromNow,
+              active: true,
+              userId: user.userId,
+            },
+          });
+
+          showRewardsDiscountNotification = true;
+          currPoints = currPoints - 1500;
+        }
+      }
+
+      // 3) create order row
+
+      // fyi: prisma already assigns the uuid of the order being created here to orderId field
+
+      // Assume orderDetails.items is an array of items that the user wants to order,
+      // and each item may have customizations.
+      const orderItemsData = orderDetails.items.map((item) => ({
+        name: item.name,
+        quantity: item.quantity,
+        price: item.price,
+        menuItemId: item.itemId,
+        specialInstructions: item.specialInstructions,
+        includeDietaryRestrictions: item.includeDietaryRestrictions,
+        customizations: {
+          create: item.customizations.map((customization) => ({
+            customizationCategoryId: customization.categoryId,
+            customizationChoiceId: customization.choiceId,
+          })),
+        },
+        discountId: item.discountId,
+      }));
+
+      const orderData = {
+        stripeSessionId: payment.id,
+        datetimeToPickup: new Date(orderDetails.dateToPickUp ?? Date.now()),
+        // ^ dateToPickup field obv shouldn't be allowed to be undefined...
+        firstName: paymentMetadata.firstName,
+        lastName: paymentMetadata.lastName,
+        email: paymentMetadata.email,
+        phoneNumber: paymentMetadata.phoneNumber,
+        prevRewardsPoints: prevPoints,
+        rewardsPoints: currPoints,
+        userId: user ? user.userId : null, // Assuming `null` is acceptable for guests
+        orderItems: {
+          create: orderItemsData, // Nested array for order items and their customizations
+        },
+      };
+
+      const order = await prisma.order.create({
+        data: orderData,
+      });
+
+      // 4) update user rewards points/rank
+      if (user) {
+        await prisma.user.update({
+          where: {
+            id: payment.metadata.userId,
+          },
+          data: {
+            rewardsPoints: currPoints,
+            showRewardsDiscountNotification,
+          },
+        });
+      }
+
+      // 5) send websocket emit to dashboard
+
+      // seems like we may have to do some extra work to get this to send to
+      // backend socket.ts
+      socket.emit("newOrderCreated");
+
+      // TODO
+
+      // 6) send post request to w/e pos system we are using
+
+      // TODO
+
+      await prisma.transientOrder.delete({
+        where: {
+          userId: payment.metadata.userId,
+        },
+      });
+
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+  res.json({ received: true });
 };
 
 export default webhook;
