35c3bb938a3a2119c06d1f8e3de5dcae49f65e74
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-05-14 15:46:58 -0500

    refactor - uses util function getFirstValidMidnightDate

--- src/server/api/routers/validateOrder.ts
+++ src/server/api/routers/validateOrder.ts
@@ -6,46 +6,7 @@ import { getMidnightDate } from "~/utils/getMidnightDate";
 import { isAbleToRenderASAPTimeSlot } from "~/utils/isAbleToRenderASAPTimeSlot";
 import { is30MinsFromDatetime } from "~/utils/is30MinsFromDatetime";
 import Decimal from "decimal.js";
-
-const holidays = [
-  new Date("2024-12-25"),
-  new Date("2024-12-26"),
-  new Date("2025-01-01"),
-];
-
-function isSundayOrMonday(date: Date) {
-  const dayOfWeek = date.getDay();
-  return dayOfWeek === 0 || dayOfWeek === 1;
-}
-
-function isHoliday(date: Date, holidays: Date[]) {
-  const dateString = date.toISOString().split("T")[0]; // Converts date to YYYY-MM-DD format
-  return holidays.some(
-    (holiday) => holiday.toISOString().split("T")[0] === dateString,
-  );
-}
-
-function validateDateToPickup(orderDetails: OrderDetails, holidays: Date[]) {
-  let datetimeToPickup = new Date(orderDetails.datetimeToPickup);
-  const now = new Date();
-  now.setHours(0, 0, 0, 0); // Normalize now to midnight for consistent comparison
-
-  // If datetimeToPickup is in the past, adjust it to midnight today
-  if (datetimeToPickup <= now) {
-    datetimeToPickup = now;
-  }
-
-  // Check and adjust for Sundays, Mondays, and Holidays
-  while (
-    isSundayOrMonday(datetimeToPickup) ||
-    isHoliday(datetimeToPickup, holidays)
-  ) {
-    datetimeToPickup.setDate(datetimeToPickup.getDate() + 1); // Move to the next day
-  }
-
-  // Update the orderDetails with the validated or adjusted date
-  orderDetails.datetimeToPickup = datetimeToPickup;
-}
+import { getFirstValidMidnightDate } from "~/utils/getFirstValidMidnightDate";
 
 function validateTimeToPickup(
   orderDetails: OrderDetails,
@@ -136,7 +97,9 @@ export const validateOrderRouter = createTRPCRouter({
 
       if (!validatingAReorder) {
         // Date validation
-        validateDateToPickup(orderDetails, holidays);
+        orderDetails.datetimeToPickup = getFirstValidMidnightDate(
+          orderDetails.datetimeToPickup,
+        );
 
         // Pickup time validation
         const dbMinOrderPickupTime =
