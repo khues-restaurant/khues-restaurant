9a7ff6f0b02d226852b3fe9d187128a8edf3ba4b
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-05-15 23:50:42 -0500

    changed "shouldScaleBackground" to default to false

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -5,7 +5,7 @@ import { Button } from "~/components/ui/button";
 import { cn } from "~/utils/shadcnuiUtils";
 
 const Drawer = ({
-  shouldScaleBackground = true,
+  shouldScaleBackground = false,
   ...props
 }: React.ComponentProps<typeof DrawerPrimitive.Root>) => (
   <DrawerPrimitive.Root
