09a0d4274e6779a496d0435369d6fb883ed31a15
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-05-20 22:59:38 -0500

    simplify + perf. upgrade - just uses orderDetails from store instead of db/localStorage

--- src/hooks/useKeepOrderDetailsValidated.ts
+++ src/hooks/useKeepOrderDetailsValidated.ts
@@ -1,23 +1,22 @@
 import { useAuth } from "@clerk/nextjs";
-import Decimal from "decimal.js";
 import { useEffect } from "react";
 import useGetUserId from "~/hooks/useGetUserId";
 import useUpdateOrder from "~/hooks/useUpdateOrder";
-import { orderDetailsSchema } from "~/stores/MainStore";
-import { useMainStore, type OrderDetails } from "~/stores/MainStore";
+import { useMainStore } from "~/stores/MainStore";
 import { api } from "~/utils/api";
-import { getFirstValidMidnightDate } from "~/utils/getFirstValidMidnightDate";
 
 function useKeepOrderDetailsValidated() {
   const userId = useGetUserId();
   const { isLoaded, isSignedIn } = useAuth();
 
   const {
+    orderDetails,
     cartInitiallyValidated,
     setOrderDetails,
     setItemNamesRemovedFromCart,
     setValidatingCart,
   } = useMainStore((state) => ({
+    orderDetails: state.orderDetails,
     cartInitiallyValidated: state.cartInitiallyValidated,
     setOrderDetails: state.setOrderDetails,
     setItemNamesRemovedFromCart: state.setItemNamesRemovedFromCart,
@@ -25,10 +24,6 @@ function useKeepOrderDetailsValidated() {
   }));
 
   const { updateOrder } = useUpdateOrder();
-
-  const { data: user } = api.user.get.useQuery(userId, {
-    enabled: Boolean(userId && isSignedIn),
-  });
 
   const { mutate: validateOrder } = api.validateOrder.validate.useMutation({
     onSuccess: (data) => {
@@ -54,63 +49,14 @@ function useKeepOrderDetailsValidated() {
   // revalidates order details on window refocus
   useEffect(() => {
     function validateOrderOnWindowRefocus() {
-      if (
-        !isLoaded ||
-        (isSignedIn && user === undefined) ||
-        userId === "" ||
-        !cartInitiallyValidated
-      )
-        return;
+      if (!isLoaded || userId === "" || !cartInitiallyValidated) return;
 
       console.log("keep order details validated ran");
 
-      const defaultCart = {
-        datetimeToPickup: getFirstValidMidnightDate(new Date()),
-        isASAP: false,
-        items: [],
-        tipPercentage: null,
-        tipValue: new Decimal(0),
-        includeNapkinsAndUtensils: false,
-        discountId: null,
-      } as OrderDetails;
-
-      if (user) {
-        try {
-          orderDetailsSchema.parse(user.currentOrder);
-
-          const parsedOrder = user.currentOrder as unknown as OrderDetails;
-
-          setValidatingCart(true);
-          validateOrder({ userId: user.userId, orderDetails: parsedOrder });
-
-          return;
-        } catch {
-          // falling back to default (empty cart) if user.currentOrder is not in valid shape
-          setValidatingCart(true);
-          validateOrder({ userId, orderDetails: defaultCart });
-
-          return;
-        }
-      }
-
-      let localStorageOrder = localStorage.getItem("khue's-orderDetails");
-
-      if (!localStorageOrder) {
-        // set local storage to default (empty cart)
-        localStorage.setItem(
-          "khue's-orderDetails",
-          JSON.stringify(defaultCart),
-        );
-
-        localStorageOrder = JSON.stringify(defaultCart);
-      }
-
-      const parsedOrder = JSON.parse(localStorageOrder) as OrderDetails;
-
-      parsedOrder.datetimeToPickup = new Date(parsedOrder.datetimeToPickup);
-
       setValidatingCart(true);
-      validateOrder({ userId, orderDetails: parsedOrder });
+      validateOrder({ userId, orderDetails });
+
+      return;
     }
 
     window.addEventListener("focus", validateOrderOnWindowRefocus);
@@ -119,11 +65,11 @@ function useKeepOrderDetailsValidated() {
       window.removeEventListener("focus", validateOrderOnWindowRefocus);
     };
   }, [
+    orderDetails,
     setOrderDetails,
     isLoaded,
     isSignedIn,
     userId,
-    user,
     validateOrder,
     setValidatingCart,
     cartInitiallyValidated,
