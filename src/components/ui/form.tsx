b41196864b3b59bc1d17e4c9cd793df5bca0315a
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-06-01 02:27:54 -0500

    transition-colors on form labels

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -93,7 +93,11 @@ const FormLabel = React.forwardRef<
   return (
     <Label
       ref={ref}
-      className={cn(error && "text-destructive", className)}
+      className={cn(
+        error && "text-destructive",
+        "transition-colors",
+        className,
+      )}
       htmlFor={formItemId}
       {...props}
     />
