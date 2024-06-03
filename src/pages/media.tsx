609114388fd10eedcafd71d061a74ae82d8db673
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-06-03 18:41:04 -0500

    major mobile/desktop ui/ux modernization refactor

--- src/pages/media.tsx
+++ src/pages/media.tsx
@@ -11,6 +11,7 @@ import mpr from "/public/media/mpr.jpg";
 import kare11Logo from "/public/media/kare11Logo.png";
 import starTribuneLogo from "/public/media/starTribuneLogo.png";
 import mprLogo from "/public/media/mprLogo.png";
+import StaticLotus from "~/components/ui/StaticLotus";
 
 function Media() {
   return (
@@ -125,23 +126,25 @@ function Media() {
         </div>
       </div>
 
-      <div className="relative grid w-80 grid-cols-1 gap-8 py-16 pb-24 tablet:w-[900px] tablet:grid-cols-2 tablet:gap-16 desktop:w-[1200px] desktop:grid-cols-3">
-        <div className="baseVertFlex h-min w-full !justify-start rounded-md border-2 border-primary shadow-md">
-          <div className="baseVertFlex relative w-full bg-primary shadow-md">
+      <div className="relative grid w-80 grid-cols-1 gap-8 py-16 pb-24 md:w-[700px] md:grid-cols-2 md:gap-8 xl:!hidden">
+        <div className="baseVertFlex h-min w-full !justify-start rounded-md border shadow-md">
+          <Button variant={"text"} asChild>
             <a
               href="https://www.kare11.com/video/life/food/recipes/kare-in-the-kitchen-fried-chicken-sandwiches-with-eric-pham-from-khues-kitchen/89-ad173b59-cea7-4fdf-b05a-3711b8c97553"
-              className="relative size-full"
+              className="baseVertFlex relative z-10 !size-full rounded-b-none rounded-t-md bg-primary !p-0 shadow-md"
             >
               <Image
                 src={kare11InTheKitchen}
                 alt="TODO"
                 sizes="(min-width: 1000px) 400px, 320px"
-                className="!relative rounded-t-sm bg-offwhite transition-all hover:brightness-90 active:brightness-75"
+                className="!relative rounded-t-md bg-offwhite transition-all hover:brightness-90 active:brightness-75"
               />
             </a>
-          </div>
+          </Button>
 
-          <div className="baseVertFlex w-full !items-start gap-2 hyphens-auto p-4 pt-3">
+          <div className="baseVertFlex relative w-full !items-start gap-2 overflow-hidden hyphens-auto rounded-b-md bg-offwhite p-4 pb-12 pt-3">
+            <StaticLotus className="absolute -bottom-6 -right-4 h-16 w-16 rotate-[-45deg] fill-primary/50" />
+            <StaticLotus className="absolute -bottom-6 -left-4 h-16 w-16 rotate-[45deg] fill-primary/50" />
             <Button variant={"text"} className="!p-0" asChild>
               <a href="https://www.kare11.com/">
                 <Image
@@ -152,7 +155,7 @@ function Media() {
                 />
               </a>
             </Button>
-            <p className="text-lg font-semibold">
+            <p className="pb-2 text-lg font-semibold">
               KARE in the Kitchen: Fried chicken sandwiches with Eric Pham from
               Khue&apos;s Kitchen
             </p>
@@ -170,22 +173,24 @@ function Media() {
           </div>
         </div>
 
-        <div className="baseVertFlex h-min w-full !justify-start rounded-md border-2 border-primary shadow-md">
-          <div className="baseVertFlex relative w-full bg-primary shadow-md">
+        <div className="baseVertFlex h-min w-full !justify-start rounded-md border shadow-md">
+          <Button variant={"text"} asChild>
             <a
               href="https://www.kare11.com/article/news/local/mpls-chef-credits-his-mom-for-inspiration/89-0f237053-85cf-48ae-96f7-8cbebb780555"
-              className="relative size-full"
+              className="baseVertFlex relative z-10 !size-full rounded-b-none rounded-t-md bg-primary !p-0 shadow-md"
             >
               <Image
                 src={kare11MothersDay}
                 alt="TODO"
                 sizes="(min-width: 1000px) 400px, 320px"
-                className="!relative rounded-t-sm bg-offwhite transition-all hover:brightness-90 active:brightness-75"
+                className="!relative rounded-t-md bg-offwhite transition-all hover:brightness-90 active:brightness-75"
               />
             </a>
-          </div>
+          </Button>
 
-          <div className="baseVertFlex w-full !items-start gap-2 hyphens-auto p-4 pt-3">
+          <div className="baseVertFlex relative w-full !items-start gap-2 overflow-hidden hyphens-auto rounded-b-md bg-offwhite p-4 pb-12 pt-3">
+            <StaticLotus className="absolute -bottom-6 -right-4 h-16 w-16 rotate-[-45deg] fill-primary/50" />
+            <StaticLotus className="absolute -bottom-6 -left-4 h-16 w-16 rotate-[45deg] fill-primary/50" />
             <Button variant={"text"} className="!p-0" asChild>
               <a href="https://www.kare11.com/">
                 <Image
@@ -196,7 +201,7 @@ function Media() {
                 />
               </a>
             </Button>
-            <p className="text-lg font-semibold">
+            <p className="pb-2 text-lg font-semibold">
               Minneapolis chef credits his mom for inspiration
             </p>
             &quot;The Quang Restaurant is synonymous with Eat Street. The
@@ -215,22 +220,24 @@ function Media() {
           </div>
         </div>
 
-        <div className="baseVertFlex h-min w-full !justify-start rounded-md border-2 border-primary shadow-md">
-          <div className="baseVertFlex relative w-full bg-primary shadow-md">
+        <div className="baseVertFlex h-min w-full !justify-start rounded-md border shadow-md">
+          <Button variant={"text"} asChild>
             <a
               href="https://www.startribune.com/how-these-moms-shaped-the-next-generation-of-great-twin-cities-restaurateurs/600273728/?refresh=true"
-              className="relative size-full"
+              className="baseVertFlex relative z-10 !size-full rounded-b-none rounded-t-md bg-primary !p-0 shadow-md"
             >
               <Image
                 src={starTribune}
                 alt="TODO"
                 sizes="(min-width: 1000px) 400px, 320px"
-                className="!relative rounded-t-sm bg-offwhite transition-all hover:brightness-90 active:brightness-75"
+                className="!relative rounded-t-md bg-offwhite transition-all hover:brightness-90 active:brightness-75"
               />
             </a>
-          </div>
+          </Button>
 
-          <div className="baseVertFlex w-full !items-start gap-2 hyphens-auto p-4 pt-2">
+          <div className="baseVertFlex relative w-full !items-start gap-2 overflow-hidden hyphens-auto rounded-b-md bg-offwhite p-4 pb-12 pt-2">
+            <StaticLotus className="absolute -bottom-6 -right-4 h-16 w-16 rotate-[-45deg] fill-primary/50" />
+            <StaticLotus className="absolute -bottom-6 -left-4 h-16 w-16 rotate-[45deg] fill-primary/50" />
             <Button variant={"text"} className="!p-0" asChild>
               <a href="https://www.startribune.com/">
                 <Image
@@ -242,7 +249,7 @@ function Media() {
                 />
               </a>
             </Button>
-            <p className="text-lg font-semibold">
+            <p className="pb-2 text-lg font-semibold">
               These 4 Twin Cities area restaurateurs learned from the best: Mom
             </p>
             &quot;For Khue Pham, the restaurant business is inextricable from
@@ -263,22 +270,24 @@ function Media() {
           </div>
         </div>
 
-        <div className="baseVertFlex h-min w-full !justify-start rounded-md border-2 border-primary shadow-md">
-          <div className="baseVertFlex relative w-full bg-primary shadow-md">
+        <div className="baseVertFlex h-min w-full !justify-start rounded-md border shadow-md">
+          <Button variant={"text"} asChild>
             <a
               href="https://www.mprnews.org/story/2023/12/27/appetites-looks-back-on-2023-restaurants-vietnamese-meatballs-and-the-secret-to-entertaining"
-              className="relative size-full"
+              className="baseVertFlex relative z-10 !size-full rounded-b-none rounded-t-md bg-primary !p-0 shadow-md"
             >
               <Image
                 src={mpr}
                 alt="TODO"
                 sizes="(min-width: 1000px) 400px, 320px"
-                className="!relative rounded-t-sm bg-offwhite transition-all hover:brightness-90 active:brightness-75"
+                className="!relative rounded-t-md bg-offwhite transition-all hover:brightness-90 active:brightness-75"
               />
             </a>
-          </div>
+          </Button>
 
-          <div className="baseVertFlex w-full !items-start gap-2 hyphens-auto p-4">
+          <div className="baseVertFlex relative w-full !items-start gap-2 overflow-hidden hyphens-auto rounded-b-md bg-offwhite p-4 pb-12">
+            <StaticLotus className="absolute -bottom-6 -right-4 h-16 w-16 rotate-[-45deg] fill-primary/50" />
+            <StaticLotus className="absolute -bottom-6 -left-4 h-16 w-16 rotate-[45deg] fill-primary/50" />
             <Button variant={"text"} className="!p-0" asChild>
               <a href="https://www.mprnews.org/">
                 <Image
@@ -290,7 +299,7 @@ function Media() {
                 />
               </a>
             </Button>
-            <p className="text-lg font-semibold">
+            <p className="pb-2 text-lg font-semibold">
               Eric Pham of the Quang Restaurant family
             </p>
             &quot;Eric Pham grew up in Minneapolis&apos; Quang Restaurant
@@ -307,6 +316,199 @@ function Media() {
               <p className="text-sm italic text-stone-400">12/27/2023</p>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="baseVertFlex relative !hidden w-[1200px] gap-16 py-16 pb-24 xl:!flex">
+        <div className="baseFlex h-min w-full rounded-md border shadow-md">
+          <div className="baseVertFlex relative w-full !items-start gap-2 overflow-hidden hyphens-auto rounded-b-md rounded-l-md bg-offwhite p-4 pb-8 pt-3">
+            <StaticLotus className="absolute -right-4 -top-6 h-16 w-16 rotate-[-135deg] fill-primary/50" />
+            <StaticLotus className="absolute -bottom-6 -left-4 h-16 w-16 rotate-[45deg] fill-primary/50" />
+            <Button variant={"text"} className="!p-0" asChild>
+              <a href="https://www.kare11.com/">
+                <Image
+                  src={kare11Logo}
+                  alt="Kare 11 Logo"
+                  width={86}
+                  height={40}
+                />
+              </a>
+            </Button>
+            <p className="pb-2 text-lg font-semibold">
+              KARE in the Kitchen: Fried chicken sandwiches with Eric Pham from
+              Khue&apos;s Kitchen
+            </p>
+            &quot;Chef Eric Pham, the chef and owner behind Khue&apos;s Kitchen,
+            whips up a delicious, hot and juicy fried chicken sandwich with KARE
+            11&apos;s Jennifer Austin.&quot;
+            <div className="baseFlex mt-2 w-full !justify-between">
+              <Button variant={"link"} className="h-8 !p-0" asChild>
+                <a href="https://www.kare11.com/video/life/food/recipes/kare-in-the-kitchen-fried-chicken-sandwiches-with-eric-pham-from-khues-kitchen/89-ad173b59-cea7-4fdf-b05a-3711b8c97553">
+                  Read more
+                </a>
+              </Button>
+              <p className="text-sm italic text-stone-400">8/13/2022</p>
+            </div>
+          </div>
+
+          <Button variant={"text"} asChild>
+            <a
+              href="https://www.kare11.com/video/life/food/recipes/kare-in-the-kitchen-fried-chicken-sandwiches-with-eric-pham-from-khues-kitchen/89-ad173b59-cea7-4fdf-b05a-3711b8c97553"
+              className="baseVertFlex relative h-[232px] w-[650px] !rounded-l-none rounded-r-md bg-primary !p-0 shadow-md"
+            >
+              <Image
+                src={kare11InTheKitchen}
+                alt="TODO"
+                sizes="(min-width: 1000px) 400px, 320px"
+                className="!relative rounded-r-md bg-offwhite transition-all hover:brightness-90 active:brightness-75"
+              />
+            </a>
+          </Button>
+        </div>
+
+        <div className="baseFlex h-min w-full rounded-md border shadow-md">
+          <div className="baseVertFlex relative w-full !items-start gap-2 overflow-hidden hyphens-auto rounded-l-md bg-offwhite p-4 pb-8 pt-3">
+            <StaticLotus className="absolute -right-4 -top-6 h-16 w-16 rotate-[-135deg] fill-primary/50" />
+            <StaticLotus className="absolute -bottom-6 -left-4 h-16 w-16 rotate-[45deg] fill-primary/50" />
+            <Button variant={"text"} className="!p-0" asChild>
+              <a href="https://www.kare11.com/">
+                <Image
+                  src={kare11Logo}
+                  alt="Kare 11 Logo"
+                  width={86}
+                  height={40}
+                />
+              </a>
+            </Button>
+            <p className="pb-2 text-lg font-semibold">
+              Minneapolis chef credits his mom for inspiration
+            </p>
+            &quot;The Quang Restaurant is synonymous with Eat Street. The
+            popular Vietnamese restaurant located in Minneapolis has been
+            serving authentic Vietnamese cuisine for over 30 years. It was
+            opened by the Pham family, who immigrated to the U.S. after the
+            Vietnam War...&quot;
+            <div className="baseFlex mt-2 w-full !justify-between">
+              <Button variant={"link"} className="h-8 !p-0" asChild>
+                <a href="https://www.kare11.com/article/news/local/mpls-chef-credits-his-mom-for-inspiration/89-0f237053-85cf-48ae-96f7-8cbebb780555">
+                  Read more
+                </a>
+              </Button>
+              <p className="text-sm italic text-stone-400">10/14/2023</p>
+            </div>
+          </div>
+
+          <Button variant={"text"} asChild>
+            <a
+              href="https://www.kare11.com/article/news/local/mpls-chef-credits-his-mom-for-inspiration/89-0f237053-85cf-48ae-96f7-8cbebb780555"
+              className="baseVertFlex relative h-[256px] w-[650px] !rounded-l-none rounded-r-md bg-primary !p-0 shadow-md"
+            >
+              <Image
+                src={kare11MothersDay}
+                alt="TODO"
+                sizes="(min-width: 1000px) 400px, 320px"
+                className="!relative h-full rounded-r-md bg-offwhite transition-all hover:brightness-90 active:brightness-75"
+              />
+            </a>
+          </Button>
+        </div>
+
+        <div className="baseFlex h-min w-full rounded-md border shadow-md">
+          <div className="baseVertFlex relative w-full !items-start gap-2 overflow-hidden hyphens-auto rounded-l-md bg-offwhite p-4 pb-8 pt-2">
+            <StaticLotus className="absolute -right-4 -top-6 h-16 w-16 rotate-[-135deg] fill-primary/50" />
+            <StaticLotus className="absolute -bottom-6 -left-4 h-16 w-16 rotate-[45deg] fill-primary/50" />
+            <Button variant={"text"} className="!p-0" asChild>
+              <a href="https://www.startribune.com/">
+                <Image
+                  src={starTribuneLogo}
+                  alt="TODO"
+                  width={150}
+                  height={35}
+                  className="-ml-5"
+                />
+              </a>
+            </Button>
+            <p className="pb-2 text-lg font-semibold">
+              These 4 Twin Cities area restaurateurs learned from the best: Mom
+            </p>
+            &quot;For Khue Pham, the restaurant business is inextricable from
+            sacrifice. Her mother, Lung Tran, was just 36 when her father,
+            Quang, died unexpectedly. With six kids to raise, there was little
+            time to sit in her grief. Instead, she built a restaurant that
+            endures as an iconic Minneapolis institution: Quang. &ldquo;I
+            remember sleeping on a cot in the restaurant because I was so tired
+            I couldn&apos;t drive,&rdquo; said Khue. She and her...&quot;
+            <div className="baseFlex mt-2 w-full !justify-between">
+              <Button variant={"link"} className="h-8 !p-0" asChild>
+                <a href="https://www.startribune.com/how-these-moms-shaped-the-next-generation-of-great-twin-cities-restaurateurs/600273728/?refresh=true">
+                  Read more
+                </a>
+              </Button>
+              <p className="text-sm italic text-stone-400">10/10/2023</p>
+            </div>
+          </div>
+
+          <Button variant={"text"} asChild>
+            <a
+              href="https://www.startribune.com/how-these-moms-shaped-the-next-generation-of-great-twin-cities-restaurateurs/600273728/?refresh=true"
+              className="baseVertFlex relative h-[300px] w-[650px] !rounded-l-none rounded-r-md bg-primary !p-0 shadow-md"
+            >
+              <Image
+                src={starTribune}
+                alt="TODO"
+                sizes="(min-width: 1000px) 400px, 320px"
+                className="!relative !h-full rounded-r-md bg-offwhite !object-cover transition-all hover:brightness-90 active:brightness-75"
+              />
+            </a>
+          </Button>
+        </div>
+
+        <div className="baseFlex h-min w-full rounded-md border shadow-md">
+          <div className="baseVertFlex relative w-full !items-start gap-2 overflow-hidden hyphens-auto rounded-l-md bg-offwhite p-4 pb-8 pt-6">
+            <StaticLotus className="absolute -right-4 -top-6 h-16 w-16 rotate-[-135deg] fill-primary/50" />
+            <StaticLotus className="absolute -bottom-6 -left-4 h-16 w-16 rotate-[45deg] fill-primary/50" />
+            <Button variant={"text"} className="!p-0" asChild>
+              <a href="https://www.mprnews.org/">
+                <Image
+                  src={mprLogo}
+                  alt="TODO"
+                  width={110}
+                  height={85}
+                  className="mb-3"
+                />
+              </a>
+            </Button>
+            <p className="pb-2 text-lg font-semibold">
+              Eric Pham of the Quang Restaurant family
+            </p>
+            &quot;Eric Pham grew up in Minneapolis&apos; Quang Restaurant
+            family. Now a chef in his own right, he talked to Tom Crann about
+            opening his own place — Khue&apos;s Kitchen, named for his mom Khue
+            Pham — and showed us how he makes his favorite dish: bánh mì xíu
+            mại, Vietnamese meatball sandwiches.&quot;
+            <div className="baseFlex mt-2 w-full !justify-between">
+              <Button variant={"link"} className="h-8 !p-0" asChild>
+                <a href="https://www.mprnews.org/story/2023/12/27/appetites-looks-back-on-2023-restaurants-vietnamese-meatballs-and-the-secret-to-entertaining">
+                  Read more
+                </a>
+              </Button>
+              <p className="text-sm italic text-stone-400">12/27/2023</p>
+            </div>
+          </div>
+
+          <Button variant={"text"} asChild>
+            <a
+              href="https://www.mprnews.org/story/2023/12/27/appetites-looks-back-on-2023-restaurants-vietnamese-meatballs-and-the-secret-to-entertaining"
+              className="baseVertFlex relative h-[268px] w-[650px] !rounded-l-none rounded-r-md bg-primary !p-0 shadow-md"
+            >
+              <Image
+                src={mpr}
+                alt="TODO"
+                sizes="(min-width: 1000px) 400px, 320px"
+                className="!relative !h-full rounded-r-md bg-offwhite object-cover transition-all hover:brightness-90 active:brightness-75"
+              />
+            </a>
+          </Button>
         </div>
       </div>
     </motion.div>
