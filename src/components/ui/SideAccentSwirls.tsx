049a5f6825a59f83dd2242b1e4a2d5078545067b
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-06-08 00:49:44 -0500

    increased horizontal viewbox width

--- src/components/ui/SideAccentSwirls.tsx
+++ src/components/ui/SideAccentSwirls.tsx
@@ -14,7 +14,7 @@ export default function SideAccentSwirls({
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       version="1.1"
-      viewBox="390 295 72 30"
+      viewBox="391 295 72 30"
       xmlSpace="preserve"
       className={className}
     >
