132232aec8ae1bf451d79ea82f74fc4c5e4f01e0
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-06-09 02:05:25 -0500

    trying more robust, methodical approach

--- src/pages/api/crons/resetDBMinPickupTime.ts
+++ src/pages/api/crons/resetDBMinPickupTime.ts
@@ -1,23 +1,40 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { fromZonedTime } from "date-fns-tz";
+import { formatInTimeZone, fromZonedTime } from "date-fns-tz";
 import { prisma } from "~/server/db";
+
+// This is set to run at "0 6 * * *"
+// which allows for daylight savings time buffer
 
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse,
 ) {
   try {
-    const startOfDayInChicago = fromZonedTime(new Date(), "America/Chicago");
-    startOfDayInChicago.setHours(0, 0, 0, 0);
+    const timeZone = "America/Chicago";
 
+    // Get the current date in the "America/Chicago" time zone
+    const nowInChicago = fromZonedTime(new Date(), timeZone);
+
+    // Set the time to midnight in the "America/Chicago" time zone
+    nowInChicago.setHours(0, 0, 0, 0);
+
+    // Convert the "America/Chicago" midnight time back to UTC
+    const startOfDayInChicagoUTC = formatInTimeZone(
+      nowInChicago,
+      "UTC",
+      "yyyy-MM-dd HH:mm:ssXXX",
+    );
+
+    // Update the database with the correct time
     await prisma.minimumOrderPickupTime.update({
       where: {
         id: 1,
       },
       data: {
-        value: startOfDayInChicago,
+        value: new Date(startOfDayInChicagoUTC),
       },
     });
+
     return response.status(200).json({ completed: true });
   } catch (error) {
     console.error("Error updating minimumOrderPickupTime:", error);
