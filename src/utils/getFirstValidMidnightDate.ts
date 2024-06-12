5d1e0d2918284f8b7f946aa3400ea38c9b340b8e
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-06-11 20:17:47 -0500

    another prod log test for verification

--- src/utils/getFirstValidMidnightDate.ts
+++ src/utils/getFirstValidMidnightDate.ts
@@ -1,12 +1,24 @@
+import { format, startOfDay } from "date-fns";
 import { toZonedTime } from "date-fns-tz";
 import { loopToFindFirstOpenDay } from "~/utils/loopToFindFirstOpenDay";
+import { setDateToMidnightCST } from "~/utils/setDateToMidnightCST";
+
+// process.env.TZ = "Asia/Calcutta";
 
 // Not the happiest about state of this function, is currently just a thin
 // wrapper around loopToFindFirstOpenDay, making sure to pass in the current
 // day at midnight. Maybe refactor later
 export function getFirstValidMidnightDate() {
-  const todayAtMidnight = toZonedTime(new Date(), "America/Chicago");
-  todayAtMidnight.setHours(0, 0, 0, 0); // Normalize now to midnight for consistent comparison
+  const todayAtMidnight = toZonedTime(
+    startOfDay(new Date()),
+    "America/Chicago",
+  );
+
+  console.log(
+    "testtesttest:",
+    format(todayAtMidnight, "yyyy-MM-dd HH:mm:ssXXX"),
+  );
+  // todayAtMidnight.setUTCHours(0, 0, 0, 0); // Normalize now to midnight for consistent comparison
 
   return loopToFindFirstOpenDay(todayAtMidnight);
 }
