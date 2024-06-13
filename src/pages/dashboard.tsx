db695838d6e02c87f963656f40e301dd9fff0354
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-06-12 22:32:06 -0500

    brought back Dashboard shell page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,8 @@
+// technically the main dashboard components should live here instead of the
+// <DashboardLayout> component right?
+
+function Dashboard() {
+  return <></>;
+}
+
+export default Dashboard;
