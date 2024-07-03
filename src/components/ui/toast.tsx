8b1816bfa6a3d2f9a38d28c3d311dd719ffc7d5b
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-07-02 20:38:28 -0500

    active animation for mobile + disable pointer events for ToastClose on mobile since hitbox is so small anyway + it doesn't render visually

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -112,7 +112,7 @@ const ToastAction = React.forwardRef<
   <ToastPrimitives.Action
     ref={ref}
     className={cn(
-      "!mr-2 inline-flex h-8 shrink-0 items-center justify-center rounded-md border bg-transparent px-3 text-sm font-medium opacity-90 ring-offset-background transition-colors hover:bg-secondary hover:text-black focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 group-[.destructive]:border-muted/40 group-[.destructive]:hover:border-destructive/30 group-[.destructive]:hover:bg-destructive group-[.destructive]:hover:text-destructive-foreground group-[.destructive]:focus:ring-destructive tablet:!mr-0",
+      "!mr-2 inline-flex h-8 shrink-0 items-center justify-center rounded-md border bg-transparent px-3 text-sm font-medium opacity-90 ring-offset-background transition-colors hover:bg-secondary hover:text-black focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 active:bg-secondary active:text-black disabled:pointer-events-none disabled:opacity-50 group-[.destructive]:border-muted/40 group-[.destructive]:hover:border-destructive/30 group-[.destructive]:hover:bg-destructive group-[.destructive]:hover:text-destructive-foreground group-[.destructive]:focus:ring-destructive tablet:!mr-0",
       className,
     )}
     {...props}
@@ -127,7 +127,7 @@ const ToastClose = React.forwardRef<
   <ToastPrimitives.Close
     ref={ref}
     className={cn(
-      "absolute right-2 top-2 rounded-md p-1 text-offwhite/70 opacity-0 transition-opacity hover:text-offwhite focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
+      "pointer-events-none absolute right-2 top-2 rounded-md p-1 text-offwhite/70 opacity-0 transition-opacity hover:text-offwhite focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600 tablet:pointer-events-auto",
       className,
     )}
     toast-close=""
