eb4be59d1f42620f7679af25256a73d3a5c7f6fe
Michael Ongaro <michael.ongaro.dev@gmail.com>
2025-03-01 03:29:15 -0600

    updated sitemap and dynamic head + created <OpenGraphTemplate>

--- src/components/DynamicHead.tsx
+++ src/components/DynamicHead.tsx
@@ -27,14 +27,6 @@ function getDynamicHeadJSON(currentPath: string): DynamicHeadJSON {
         ogTitle: "Menu | Khue's",
         ogUrl: "https://www.khueskitchen.com/menu",
       };
-    case "/order":
-      return {
-        title: "Order | Khue's",
-        description:
-          "Order directly from Khue's for exclusive benefits, including our lowest menu prices, priority processing, and rewards points towards free meals.",
-        ogTitle: "Order | Khue's",
-        ogUrl: "https://www.khueskitchen.com/order",
-      };
     case "/reservations":
       return {
         title: "Reservations | Khue's",
@@ -42,14 +34,6 @@ function getDynamicHeadJSON(currentPath: string): DynamicHeadJSON {
           "Secure your reservations at Khue's Kitchen. Learn about our policies for larger parties and how to guarantee your spot for an unforgettable dining experience.",
         ogTitle: "Reservations | Khue's",
         ogUrl: "https://www.khueskitchen.com/reservations",
-      };
-    case "/rewards":
-      return {
-        title: "Rewards | Khue's",
-        description:
-          "Become a member of Khue's Rewards and enjoy exclusive benefits, from earning points towards free meals to special birthday treats and early access to new dishes.",
-        ogTitle: "Rewards | Khue's",
-        ogUrl: "https://www.khueskitchen.com/rewards",
       };
     case "/our-story":
       return {
@@ -67,20 +51,6 @@ function getDynamicHeadJSON(currentPath: string): DynamicHeadJSON {
         ogTitle: "Media | Khue's",
         ogUrl: "https://www.khueskitchen.com/media",
       };
-    case "/payment-success":
-      return {
-        title: "Payment success | Khue's",
-        ogTitle: "Payment success | Khue's",
-        ogUrl: "https://www.khueskitchen.com/payment-sucess",
-        robots: "noindex,nofollow",
-      };
-    case "/track":
-      return {
-        title: "Track | Khue's",
-        ogTitle: "Track | Khue's",
-        ogUrl: "https://www.khueskitchen.com/track",
-        robots: "noindex,nofollow",
-      };
     case "/privacy":
       return {
         title: "Privacy | Khue's",
@@ -88,51 +58,6 @@ function getDynamicHeadJSON(currentPath: string): DynamicHeadJSON {
           "Khue's is committed to protecting your privacy. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website, use our services, and interact with our platform.",
         ogTitle: "Privacy | Khue's",
         ogUrl: "https://www.khueskitchen.com/privacy",
-      };
-    case "unsubscribe":
-      return {
-        title: "Unsubscribe | Khue's",
-        description:
-          "Unsubscribe from Khue's email list to stop receiving updates on new dishes, special promotions, and exclusive events.",
-        ogTitle: "Unsubscribe | Khue's",
-        ogUrl: "https://www.khueskitchen.com/unsubscribe",
-        robots: "noindex,nofollow",
-      };
-
-    // profile routes
-    case "/profile/preferences":
-      return {
-        title: "Preferences | Khue's",
-        description:
-          "Manage your Khue's account preferences, including email subscriptions, notifications, and personal information.",
-        ogTitle: "Preferences | Khue's",
-        ogUrl: "https://www.khueskitchen.com/profile/preferences",
-        robots: "noindex,nofollow",
-      };
-    case "/profile/rewards":
-      return {
-        title: "Rewards | Khue's",
-        description:
-          "Track your Khue's Rewards points, redeem rewards, and view your rewards history.",
-        ogTitle: "Rewards | Khue's",
-        ogUrl: "https://www.khueskitchen.com/profile/rewards",
-        robots: "noindex,nofollow",
-      };
-    case "/profile/my-orders":
-      return {
-        title: "My orders | Khue's",
-        description:
-          "View your past orders, track your current orders, and access your order history.",
-        ogTitle: "My orders | Khue's",
-        ogUrl: "https://www.khueskitchen.com/profile/my-orders",
-        robots: "noindex,nofollow",
-      };
-
-    // dashboard
-    case "/dashboard":
-      return {
-        title: "Dashboard | Khue's",
-        robots: "noindex,nofollow",
       };
 
     default:
@@ -161,6 +86,9 @@ function DynamicHead({ currentPath }: DynamicHead) {
       )}
       {dynamicHeadJSON.description && (
         <meta name="description" content={dynamicHeadJSON.description} />
+      )}
+      {dynamicHeadJSON.ogUrl && (
+        <link rel="canonical" href={dynamicHeadJSON.ogUrl} />
       )}
       {dynamicHeadJSON.ogTitle && (
         <meta property="og:title" content={dynamicHeadJSON.ogTitle}></meta>
@@ -191,18 +119,15 @@ function DynamicHead({ currentPath }: DynamicHead) {
       <meta property="og:type" content="website" />
       <meta
         property="og:image"
-        // TODO: Change for production
-        content={
-          "https://khues-restaurant.vercel.app/openGraph/opengraphImage.png"
-        }
+        content={"https://www.khueskitchen.com/openGraph/opengraphImage.png"}
       ></meta>
       <meta
         property="og:image:alt"
-        content="Welcome to Khue's - A modern take on classic Vietnamese cuisine. The image features a welcoming character in traditional Vietnamese attire, set against a background of delicious Vietnamese dishes."
+        content="Welcome to Khue's - A modern take on classic Vietnamese cuisine. The image features a welcoming character in traditional Vietnamese attire, set against a background showing Chef Eric Pham and his mother, Khue Pham."
       ></meta>
       <meta
         property="twitter:image"
-        content="https://khues-restaurant.vercel.app/openGraph/opengraphImage.png"
+        content="https://www.khueskitchen.com/openGraph/opengraphImage.png"
       />
       <meta property="twitter:card" content="summary_large_image" />
     </Head>
---
+++ src/components/ui/openGraphTemplate.tsx
@@ -0,0 +1,119 @@
+import { Clock, MapPin } from "lucide-react";
+import Image from "next/image";
+
+import khuesKitchenLogo from "/public/logos/khuesKitchenLogo.png";
+
+import first from "/public/openGraph/first.jpg";
+import second from "/public/openGraph/second.jpg";
+import third from "/public/openGraph/third.webp";
+import fourth from "/public/openGraph/fourth.webp";
+
+function OpenGraphTemplate() {
+  return (
+    <div className="baseVertFlex relative h-[630px] w-[1200px] ">
+      <div className="z-1 absolute left-0 top-0 grid size-full grid-cols-2 grid-rows-2">
+        <div className="relative">
+          <Image
+            src={first}
+            alt={"TODO: fill in w/ appropriate alt text"}
+            priority
+            fill
+            className="object-cover"
+          />
+        </div>
+
+        <div className="relative">
+          <Image
+            src={second}
+            alt={"TODO: fill in w/ appropriate alt text"}
+            priority
+            fill
+            className="object-cover"
+          />
+        </div>
+
+        <div className="relative">
+          <Image
+            src={third}
+            alt={"TODO: fill in w/ appropriate alt text"}
+            priority
+            fill
+            className="object-cover"
+          />
+        </div>
+
+        <div className="relative">
+          <Image
+            src={fourth}
+            alt={"TODO: fill in w/ appropriate alt text"}
+            priority
+            fill
+            className="object-cover"
+          />
+        </div>
+      </div>
+
+      <div className="z-2 absolute left-0 top-0 size-full bg-gradient-to-br from-primary/60 to-darkPrimary/60"></div>
+
+      <div className="baseFlex z-3 relative gap-12 rounded-md border bg-offwhite px-12 py-8 shadow-lightHeroContainer">
+        <Image
+          src={khuesKitchenLogo}
+          alt={"TODO: fill in w/ appropriate alt text"}
+          priority
+          className="h-[228px] w-[120.75px] drop-shadow-md"
+        />
+        <div className="baseVertFlex !items-start gap-1 rounded-md">
+          {/* experimenting with stone-800 instead of black */}
+          <h1 className="text-3xl font-bold text-stone-800 tablet:text-4xl">
+            Welcome to Khue&apos;s
+          </h1>
+          <p className=" w-[350px] text-xl text-stone-500 tablet:text-2xl">
+            A modern take on classic Vietnamese cuisine.
+          </p>
+
+          {/* <div className="baseFlex mt-6 gap-4">
+            <Button size={"lg"} asChild>
+              <Link
+                href="/menu"
+                className="baseFlex gap-2 !px-4 !py-6 !text-lg shadow-md"
+              >
+                <SideAccentSwirls
+                  delay={1.6}
+                  className="h-4 scale-x-[-1] fill-offwhite"
+                />
+                View our menu
+                <SideAccentSwirls delay={1.6} className="h-4 fill-offwhite" />
+              </Link>
+            </Button>
+
+            <Button size={"lg"} variant={"outline"} asChild>
+              <a
+                href="https://reservations.shift4payments.com/#/1814c327-a884-4b86-bc26-915ce9eadbb8"
+                className="baseFlex gap-3 !px-8 !py-6 !text-lg shadow-sm"
+              >
+                Make a reservation
+                <IoCalendarOutline />
+              </a>
+            </Button>
+          </div> */}
+
+          <div className="baseVertFlex mt-4 !items-start gap-4">
+            <div className="baseFlex gap-2">
+              <Clock className="size-5 text-primary" />
+              <p className="text-sm text-stone-500">
+                Open 4:30pm - 9:30pm Wed-Sat
+              </p>
+            </div>
+
+            <div className="baseFlex gap-2">
+              <MapPin className="size-5 text-primary" />
+              <p className="text-sm text-stone-500">St. Anthony Park, MN</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OpenGraphTemplate;
