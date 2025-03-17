5314b986a1322866e41ab91e49d7a31448a155e9
Michael Ongaro <michael.ongaro.dev@gmail.com>
2025-03-17 03:12:16 -0500

    updated to use proper clerk methods

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,24 +2,25 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { clerkClient } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isDashboardRoute = createRouteMatcher(["/dashboard(.*)"]);
 const isProfileRoute = createRouteMatcher(["/profile(.*)"]);
+const isDashboardRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const { redirectToSignIn, userId } = auth();
+  const { userId } = await auth();
 
   // Restrict profile routes to signed in users
   if (isProfileRoute(req)) {
-    auth().protect();
+    await auth.protect();
   }
 
   // Restrict admin route to users with specific role within privateMetadata
   else if (isDashboardRoute(req)) {
-    if (!userId) {
-      return redirectToSignIn();
+    if (userId === null) {
+      await auth.protect();
+      return;
     }
 
-    const user = await clerkClient().users.getUser(userId);
+    const user = await (await clerkClient()).users.getUser(userId);
 
     if (
       typeof user.privateMetadata !== "object" ||
